82988558c930f01a6f91487b260e613cc2d3d85a
lenesvc <silvas.rosilene@bol.com.br>
2017-05-29 10:21:26 +0000

    parallax

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@
          c.scrollTop() >= b ? a("#navbar-main").removeClass("navbar-fixed-top").addClass("navbar-fixed-top") : a("#navbar-main").removeClass("navbar-fixed-top")
      })
  });
+ //=====parallax
+$('div.bg-parallax').each(function(){
+			var $obj = $(this);
+		 
+			$(window).scroll(function() {
+				var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+		 
+				var bgpos = '50% '+ yPos + 'px';
+		 
+				$obj.css('background-position', bgpos );
+		 
+			}); 
+		});
+		
+		// $('.parallax-window').parallax({imageSrc: 'img/parallax.jpg',speed: 0.5});
